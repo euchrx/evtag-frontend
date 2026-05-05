bf637e96b29a02251def53857b5aebb92370d4e9
Christian <chriismatos@icloud.com>
2026-05-05 13:50:58 -0300

    update

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { AppLayout } from './../layouts/AppLayout';
 
+import { ProtectedRoute } from './ProtectedRoute';
+
+import { LoginPage } from './../pages/Login/LoginPage';
 import { DashboardPage } from './../pages/Dashboard/DashboardPage';
 import { CategoriesPage } from './../pages/Categories/CategoriesPage';
 import { ItemsPage } from './../pages/Items/ItemsPage';
@@ -15,21 +18,27 @@ import { UsersPage } from './../pages/Users/UsersPage';
 export function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
 
-      <Route element={<AppLayout />}>
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/categories" element={<CategoriesPage />} />
-        <Route path="/items" element={<ItemsPage />} />
-        <Route path="/print" element={<PrintPage />} />
-        <Route path="/history" element={<HistoryPage />} />
-        <Route path="/scan" element={<ScanPage />} />
-        <Route path="/kitchen" element={<KitchenPage />} />
-        <Route path="/devices" element={<DevicesPage />} />
-        <Route path="/companies" element={<CompaniesPage />} />
-        <Route path="/users" element={<UsersPage />} />
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route path="/kitchen" element={<KitchenPage />} />
+
+      <Route element={<ProtectedRoute />}>
+        <Route element={<AppLayout />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/categories" element={<CategoriesPage />} />
+          <Route path="/items" element={<ItemsPage />} />
+          <Route path="/print" element={<PrintPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/scan" element={<ScanPage />} />
+          <Route path="/devices" element={<DevicesPage />} />
+          <Route path="/companies" element={<CompaniesPage />} />
+          <Route path="/users" element={<UsersPage />} />
+        </Route>
       </Route>
 
-      <Route path="*" element={<Navigate to="/dashboard" replace />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
